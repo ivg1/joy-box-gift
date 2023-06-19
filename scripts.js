a34d1dc6e3b989cfc9c9a68f2d21e6b49a2e2d3e
ivg1 <132875824+ivg1@users.noreply.github.com>
2023-06-19 21:27:09 +0300

    index.html

--- scripts.js
+++ scripts.js
@@ -1,1 +1,21 @@
+window.onload = function(){
+    console.warn("the window finally loaded");
+    moveExamples();
+}
 
+function moveExamples(){
+    const positions = [
+        {
+            x: 200,
+            y: 200,
+        },
+        {
+            x: 300,
+            y: 300,
+        },
+        {
+            x: 400,
+            y: 200,
+        },
+    ];
+}
